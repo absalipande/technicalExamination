c2ff8ae9497674ceae18cd1e504e0543d41ac199
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-04 12:29:18 +0800

    feat: added a loginform and logoutbutton

---
+++ frontend/frontend/src/components/LoginForm.jsx
@@ -0,0 +1,84 @@
+import React, { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from 'axios';
+
+const LoginForm = () => {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [error, setError] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    setLoading(true);
+    setError(null);
+    const data = { username, password };
+
+    try {
+      const response = await axios.post(
+        'http://localhost:3060/Account/SignIn',
+        data
+      );
+      if (response.status === 200) {
+        setLoading(false);
+        localStorage.setItem('token', response.data.token);
+        navigate('/');
+      } else {
+        throw new Error(
+          response.data.error || 'Something went wrong, please try again later'
+        );
+      }
+    } catch (error) {
+      console.error(error);
+      setError(error.message);
+      setLoading(false);
+    }
+  };
+  return (
+    <div className='bg-gray-800 flex flex-col justify-center h-screen'>
+      <form
+        onSubmit={handleSubmit}
+        className='max-w-[400px] w-full mx-auto rounded-lg bg-gray-900 p-8 px-8'
+      >
+        <div className='flex flex-col text-white py-2'>
+          <label htmlFor='username'>Username</label>
+          <input
+            type='text'
+            className='rounded-lg bg-gray-700 mt-2 p-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none'
+            id='username'
+            value={username}
+            onChange={(event) => setUsername(event.target.value)}
+          />
+        </div>
+        <div className='flex flex-col text-white py-2'>
+          <label htmlFor='password'>Password</label>
+          <input
+            className='p-2 rounded-lg bg-gray-700 mt-2 focus:border-blue-500 focus:bg-gray-800 focus:outline-none'
+            type='password'
+            id='password'
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+        </div>
+        <div className='flex justify-between text-white py-2'>
+          <p className='flex items-center'>
+            <input className='mr-2' type='checkbox' /> Remember Me
+          </p>
+          <p>Forgot Password</p>
+        </div>
+        {error && (
+          <div className='bg-red-500 py-2 text-white text-center'>{error}</div>
+        )}
+        <button
+          disabled={loading}
+          type='submit'
+          className='w-full my-5 py-2 bg-teal-500 shadow-lg shadow-teal-500/50 hover:shadow-teal-500/40 text-white font-semibold rounded-lg'
+        >
+          {loading ? 'Loading...' : 'Submit'}
+        </button>
+      </form>
+    </div>
+  );
+};
+export default LoginForm;
---
+++ frontend/frontend/src/components/LogoutButton.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
+
+const LogoutButton = () => {
+  const navigate = useNavigate();
+
+  const handleLogout = () => {
+    localStorage.removeItem('token');
+    navigate('/Account/SignIn');
+  };
+
+  return (
+    <div className='flex justify-end'>
+      <button
+        onClick={handleLogout}
+        className='bg-teal-500 hover:bg-teal-700 text-white font-semibold py-2 px-4 rounded-lg shadow-teal-500/50 hover:shadow-teal-500/40'
+      >
+        Logout
+      </button>
+    </div>
+  );
+};
+
+export default LogoutButton;
