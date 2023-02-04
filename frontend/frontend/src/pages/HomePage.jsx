1550ec5fb9316bcb82ab66dbfdddd597330b5f2a
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-04 17:31:26 +0800

    fix: idk how to fix this

--- frontend/frontend/src/pages/HomePage.jsx
+++ frontend/frontend/src/pages/HomePage.jsx
@@ -1,33 +1,74 @@
-import React, { createContext, useEffect, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TerritoryCard from '../components/TerritoryCard';
-
-export const AllTerritoriesContext = createContext();
+import TerritoryComponent from '../components/TerritoryComponent';
 
 const HomePage = () => {
-  const [territories, setTerritories] = useState([]);
+  const [territories, setTerritories] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [openTerritories, setOpenTerritories] = useState([]);
 
   useEffect(() => {
-    const fetchTerritories = async () => {
+    const fetchData = async () => {
       try {
         const response = await axios.get(
-          'https://netzwelt-devtest.azurewebsites.net/Territories/All'
+          'http://localhost:3060/Territories/All'
         );
+        console.log(response.data);
         setTerritories(response.data);
+        setLoading(false);
       } catch (error) {
-        console.log(error);
+        console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
-    fetchTerritories();
+    fetchData();
   }, []);
 
+  const handleTerritoryClick = (id) => {
+    setOpenTerritories((prevOpenTerritories) => {
+      if (prevOpenTerritories.includes(id)) {
+        return prevOpenTerritories.filter((openId) => openId !== id);
+      } else {
+        return [...prevOpenTerritories, id];
+      }
+    });
+  };
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  if (territories.length === 0) {
+    return <p>No territories found</p>;
+  }
+
   return (
-    <div>
-      <AllTerritoriesContext.Provider value={territories}>
-        {territories.map(territory => (
-          <TerritoryCard key={territory.id} territory={territory} />
-        ))}
-      </AllTerritoriesContext.Provider>
+    <div className='bg-gray-200 p-6'>
+      <h1 className='text-xl font-bold'>Territories</h1>
+      <p className='mt-2'>Here are the list of territories:</p>
+      <div className='mt-2'>
+        {console.log(territories)}
+        {territories.length ? (
+          territories
+            .filter((territory) => !territory.parent_id)
+            .map((territory) => (
+              <TerritoryComponent
+                key={territory.id}
+                territory={{
+                  ...territory,
+                  children: territories.filter(
+                    (child) => child.parent_id === territory.id
+                  ),
+                }}
+                onClick={handleTerritoryClick}
+                open={openTerritories.includes(territory.id)}
+              />
+            ))
+        ) : (
+          <p>No territories found</p>
+        )}
+      </div>
     </div>
   );
 };
