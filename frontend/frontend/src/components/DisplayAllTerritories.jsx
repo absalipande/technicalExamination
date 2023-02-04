69474e6bd9e1446d748c91a91db91dc726e86e77
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-04 14:15:09 +0800

    feat: additional features

--- frontend/frontend/src/components/DisplayAllTerritories.jsx
+++ frontend/frontend/src/components/DisplayAllTerritories.jsx
@@ -1,9 +1,35 @@
-import React from 'react'
+import React, { useContext } from 'react';
+import LogoutButtonComponent from './LogoutButtonComponent';
+import { AllTerritoriesContext } from '../pages/HomePage';
 
-function DisplayAllTerritories() {
+const DisplayAllTerritories = () => {
+  const data = useContext(AllTerritoriesContext);
+
+  const renderTerritories = (territories) => {
+    return territories.map((territory) => {
+      return (
+        <MainTerritory
+          key={territory.id}
+          name={territory.name}
+          parent={territory.parent}
+          data={data}
+        />
+      );
+    });
+  };
+
   return (
-    <div>DisplayAllTerritories</div>
-  )
-}
+    <>
+      <div>
+        <section>
+          <h2>All TERRITORIES</h2>
+          <p>List of territories</p>
+        </section>
+        <ul>{renderTerritories(data)}</ul>
+      </div>
+      <LogoutButtonComponent />
+    </>
+  );
+};
 
-export default DisplayAllTerritories
+export default DisplayAllTerritories;
--- frontend/frontend/src/pages/HomePage.jsx
+++ frontend/frontend/src/pages/HomePage.jsx
@@ -1,21 +1,30 @@
-import React, { useEffect, useState } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import DisplayAllTerritories from '../components/DisplayAllTerritories';
+
+export const AllTerritoriesContext = createContext();
 
 const HomePage = () => {
-  const [data, setData] = useState([]);
+  const [territories, setTerritories] = useState([]);
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await fetch('http://localhost:3060/Territories/All');
         const result = await response.json();
-        setData(result.data);
+        setTerritories(result.data);
       } catch (err) {
         console.log(err);
       }
     };
     fetchData();
   }, []);
-  return <div>HomePage</div>;
+  return (
+    <div>
+      <AllTerritoriesContext>
+        <DisplayAllTerritories />
+      </AllTerritoriesContext>
+    </div>
+  );
 };
 
 export default HomePage;
