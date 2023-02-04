b58f838dd03af159b5f702cbad16b2361e9f86a8
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-04 18:12:24 +0800

    fix: whatever fix is this

--- frontend/frontend/src/components/TerritoryComponent.jsx
+++ frontend/frontend/src/components/TerritoryComponent.jsx
@@ -1,28 +1,28 @@
 import React from 'react';
 
 const TerritoryComponent = ({ territory, onClick, open }) => {
-    return (
-      <div className="flex flex-col">
-        <div
-          className="text-lg cursor-pointer"
-          onClick={() => onClick(territory.id)}
-        >
-          {open ? '▼' : '►'} {territory.name}
-        </div>
-        {open && (
-          <div className="ml-4">
-            {territory.children.map((child) => (
-              <TerritoryComponent
-                key={child.id}
-                territory={child}
-                onClick={onClick}
-                open={false}
-              />
-            ))}
-          </div>
-        )}
+  return (
+    <div className='flex flex-col'>
+      <div
+        className='text-lg cursor-pointer'
+        onClick={() => onClick(territory.id)}
+      >
+        {open ? '▼' : '►'} {territory.name}
       </div>
-    );
-  };
-  
+      {open && (
+        <div className='ml-4'>
+          {territory.children.map((child) => (
+            <TerritoryComponent
+              key={child.id}
+              territory={child}
+              onClick={onClick}
+              open={false}
+            />
+          ))}
+        </div>
+      )}
+    </div>
+  );
+};
+
 export default TerritoryComponent;
--- frontend/frontend/src/pages/HomePage.jsx
+++ frontend/frontend/src/pages/HomePage.jsx
@@ -3,7 +3,7 @@ import axios from 'axios';
 import TerritoryComponent from '../components/TerritoryComponent';
 
 const HomePage = () => {
-  const [territories, setTerritories] = useState(null);
+  const [territories, setTerritories] = useState([]);
   const [loading, setLoading] = useState(true);
   const [openTerritories, setOpenTerritories] = useState([]);
 
@@ -13,13 +13,10 @@ const HomePage = () => {
         const response = await axios.get(
           'http://localhost:3060/Territories/All'
         );
-        console.log(response.data);
-        setTerritories(response.data);
+        setTerritories(Object.values(response.data));
         setLoading(false);
       } catch (error) {
         console.error(error);
-      } finally {
-        setLoading(false);
       }
     };
     fetchData();
@@ -39,7 +36,7 @@ const HomePage = () => {
     return <p>Loading...</p>;
   }
 
-  if (territories.length === 0) {
+  if (!territories || territories.length === 0) {
     return <p>No territories found</p>;
   }
 
@@ -48,26 +45,21 @@ const HomePage = () => {
       <h1 className='text-xl font-bold'>Territories</h1>
       <p className='mt-2'>Here are the list of territories:</p>
       <div className='mt-2'>
-        {console.log(territories)}
-        {territories.length ? (
-          territories
-            .filter((territory) => !territory.parent_id)
-            .map((territory) => (
-              <TerritoryComponent
-                key={territory.id}
-                territory={{
-                  ...territory,
-                  children: territories.filter(
-                    (child) => child.parent_id === territory.id
-                  ),
-                }}
-                onClick={handleTerritoryClick}
-                open={openTerritories.includes(territory.id)}
-              />
-            ))
-        ) : (
-          <p>No territories found</p>
-        )}
+        {territories
+          .filter((territory) => !territory.parent_id)
+          .map((territory) => (
+            <TerritoryComponent
+              key={territory.id}
+              territory={{
+                ...territory,
+                children: territories.filter(
+                  (child) => child.parent_id === territory.id
+                ),
+              }}
+              onClick={handleTerritoryClick}
+              open={openTerritories.includes(territory.id)}
+            />
+          ))}
       </div>
     </div>
   );
