db741d133e187c8ce247a1a8bbe3d2916c82f324
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-04 14:02:33 +0800

    fix: forgot to return inside the App.jsx

--- frontend/frontend/src/App.jsx
+++ frontend/frontend/src/App.jsx
@@ -2,15 +2,25 @@ import React from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 import LoginPage from './pages/LoginPage';
+import HomePage from './pages/HomePage';
 import AuthComponent from './AuthComponent';
 
 const App = () => {
-  <BrowserRouter>
-    <Routes>
-      <Route path='/Account/SignIn' element={<LoginPage />} />
-    </Routes>
-    ;
-  </BrowserRouter>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/Account/SignIn' element={<LoginPage />} />
+        <Route
+          path='/'
+          element={
+            <AuthComponent>
+              <HomePage />
+            </AuthComponent>
+          }
+        />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
--- frontend/frontend/src/pages/LoginPage.jsx
+++ frontend/frontend/src/pages/LoginPage.jsx
@@ -3,7 +3,7 @@ import LoginFormComponent from '../components/LoginFormComponent';
 
 const LoginPage = () => {
   return (
-    <div className='bg-gray-500 w-screen h-screen text-white flex justify-center items-center'>
+    <div>
       <LoginFormComponent />
     </div>
   );
