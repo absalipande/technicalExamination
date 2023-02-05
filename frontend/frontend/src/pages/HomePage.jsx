394224de7c11ab978a9d67875f3d03b19a00a6e9
Amiel Brencis Salipande <absalipande@gmail.com>
2023-02-05 14:33:40 +0800

    fix: removed a component

--- frontend/frontend/src/pages/HomePage.jsx
+++ frontend/frontend/src/pages/HomePage.jsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
-import TerritoryComponent from '../components/TerritoryComponent';
 
 const HomePage = () => {
   const [territories, setTerritories] = useState([]);
